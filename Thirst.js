2ff6dc93d4636997824be91875b46d20922ab7f5
Josh <Joshualawson8@gmail.com>
2018-09-14 22:36:40 -0700

    Javascript to rotate in slideshow, TODO

---
+++ Thirst.js
@@ -0,0 +1,27 @@
+
+
+var rotation = 5;
+var position = 0;
+var items = new array();
+items[0] = "<a href='link.htm' ><img alt='image0 (9K)' src=' ThirstPictures/bubble-tea.jpg' height='300' width='300' border='0' /></a>";
+
+
+function rotater() {
+    document.getElementById("placeholder").innerHTML = items[current];
+    current = (current==items.length-1) ? 0 : current + 1;
+    setTimeout("rotater()",howOften*1000);
+}
+
+function rotater() {
+    if(document.layers) {
+        document.placeholderlayer.document.write(items[current]);
+        document.placeholderlayer.document.close();
+    }
+    if(ns6)document.getElementById("placeholderdiv").innerHTML=items[current]
+        if(document.all)
+            placeholderdiv.innerHTML=items[current];
+
+    current = (current==items.length-1) ? 0 : current + 1; //increment or reset
+    setTimeout("rotater()",howOften*1000);
+}
+window.onload=rotater;
